6777bab5ebb78f2cbd7c7cb6999bfc57cbbf6b2c
Hiroyuki Sano <sh19910711@gmail.com>
2022-05-10 02:03:53 +0000

    wip

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+typedef long long int_t;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    // cout << setprecision(12);
+
+    return 0;
+}
